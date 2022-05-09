49924b49fb725d58aa5c629ee862b959f4833f5e
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-09 12:09:46 +0530

    Render conditional styling and display item count

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -5,16 +5,21 @@ function Cart({ cart }) {
   return (
     <>
       <h2>Cart</h2>
-      <div className="items">
-        {cart.map((book) => (
-          <figure key={book.id}>
-            <Book book={book} />
-            <Button id={book.id} handleClick={() => ``}>
-              Remove from Cart
-            </Button>
-          </figure>
-        ))}
-      </div>
+      {cart && cart.length > 0 ? (
+        <div className="items">
+          <p>You have {cart.length} item(s) in your cart</p>
+          {cart.map((book) => (
+            <figure key={book.id}>
+              <Book book={book} />
+              <Button id={book.id} handleClick={() => ``}>
+                Remove from Cart
+              </Button>
+            </figure>
+          ))}
+        </div>
+      ) : (
+        <h3>Your cart is empty!</h3>
+      )}
     </>
   );
 }
