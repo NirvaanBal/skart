c90dbe11caeb9b5bfa1e67e5828a4215dc320d58
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-10 14:49:14 +0530

    Add styles to NotFound route

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 function NotFound() {
   return (
     <>
-      <h1>404</h1>
+      <h2 style={{ marginTop: '100px' }}>404</h2>
       <p>
         The page you are looking for does not exist.{' '}
-        <Link to="/">Visit Home page.</Link>
+        <Link to="/">Visit Home </Link>page.
       </p>
     </>
   );
